fa43619971506e7ac4861eebd1d7a3e6476e577a
Noah SH <sujung27@coupang.com>
2021-06-20 22:23:11 +0900

    [Conf] Add devtools

--- config-service/build.gradle.kts
+++ config-service/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-config-server")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
